c7e2f638830bae8b050d84121b34d95bf455f271
나종석 <njs60201@naver.com>
2020-09-01 00:01:05 +0900

    동적프로그래밍 연습중

--- 이것이 취업을 코딩 테스트다 with 파이썬/DP-다이나믹프로그래밍/피보나치.py
+++ 이것이 취업을 코딩 테스트다 with 파이썬/DP-다이나믹프로그래밍/피보나치.py
@@ -1,10 +1,12 @@
 
-d = [0] * 1000
 
+# 탑 다운 방식으로 피보나치 수열 작성
+
+d = [0] * 1000
 
 def fibo(x):
     if x == 1 or x == 2:
-        return 1
+        return 13
     if d[x] != 0:
         return d[x]
 
@@ -12,3 +14,19 @@ def fibo(x):
     return d[x]
 
 print(fibo(999))
+
+
+# 보텀업 방식 으로 피보나치 수열 작성
+
+
+d = [0] * 1000
+
+d[1] = 1
+d[2] = 2
+
+n = 99
+
+for i in range(3, n+1):
+    d[i] = d[i-1] + d[i-2]
+
+print(d[n])
